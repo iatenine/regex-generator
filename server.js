89d540359d28cb044124cb43ef785bd0bafe282e
Jack Linhart <iatenine@gmail.com>
2021-03-15 23:33:49 -0600

    name corrected in server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/regex-gen'));
+app.use(express.static('./dist/regex-generator'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/regex-gen/'}),
+    res.sendFile('index.html', {root: 'dist/regex-generator/'}),
 );
 
 // Start the app by listening on the default Heroku port
